d1d31760749717367100873abd6955df0868b588
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-19 19:20:49 -0400

    Init commit, regex added, api mapped, basic card design

--- weight-lifting-tracker/src/App.js
+++ weight-lifting-tracker/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route} from 'react-router-dom';
+import TabNav from './components/TabNav.js';
+import Header from './components/Header.js';
+import ExerciseList from './components/ExerciseList.js';
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <TabNav />
+      <Route exact path='/' component={ExerciseList} />
+    </main>
   );
 }
-
-export default App;
