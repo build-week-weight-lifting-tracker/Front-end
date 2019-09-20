246d7f6567ac13f01dbafc1873c9d10db7263bae
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-20 19:05:27 -0400

    Added table to UserWorkout using UserTable function

--- weight-lifting-tracker/src/components/UserWorkout.js
+++ weight-lifting-tracker/src/components/UserWorkout.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+function UserTable(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name data</td>
+          <td>Username data</td>
+          <td>
+            <button className="button muted-button">Edit</button>
+            <button className="button muted-button">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
 export default function UserWorkout(props) {
   return (
     <div className="container">
@@ -10,6 +34,7 @@ export default function UserWorkout(props) {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable />
         </div>
       </div>
     </div>
