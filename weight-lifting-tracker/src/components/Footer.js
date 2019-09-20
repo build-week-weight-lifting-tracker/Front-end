5cb2d84d62123a943afac8f9fa14ae4f29573bb1
adinkins2019 <afdinki@gmail.com>
2019-09-20 19:37:08 -0400

    added background color to Footer

--- weight-lifting-tracker/src/components/Footer.js
+++ weight-lifting-tracker/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 const newFooter = styled.footer`
-    background-color: 	rgb(222,184,135)
+    background-color: 	rgb(222,184,135);
 `
 export default function Footer(){
     return(
