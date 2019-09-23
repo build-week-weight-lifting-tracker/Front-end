697f851320767a51a2fbfbda4f50a95af2ca558d
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-23 09:51:06 -0400

    Finished with Edit User Form -- noticing an issue with editing between users -- fixing that

--- weight-lifting-tracker/src/components/UserWorkout.js
+++ weight-lifting-tracker/src/components/UserWorkout.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import UserTable from './UserTable.js';
-import AddUserForm from './AddUserForm.js'
+import AddUserForm from './AddUserForm.js';
+import EditUserForm from './EditUserForm.js';
 
 export default function UserWorkout(props) {
 
@@ -9,29 +10,52 @@ export default function UserWorkout(props) {
     { id: 2, name: 'Craig', username: 'siliconeidolon' },
     { id: 3, name: 'Ben', username: 'benisphere' },
   ];
+  const initialFormState = { id: null, name: '', username: '' };
 
   const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
 
   const addUser = user => {
     user.id = users.length + 1
     setUsers([...users, user])
   };
-
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  };
   const deleteUser = id => {
     setUsers(users.filter(user => user.id !== id))
+  };
+  const updateUser = (id, updatedUser) => {
+  setEditing(false)
+  setUsers(users.map(user => (user.id === id ? updatedUser : user)))
   };
 
   return (
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
-        </div>
+      <div className="flex-large">
+        {editing ? (
+          <div>
+            <h2>Edit user</h2>
+            <EditUserForm
+              editing={editing}
+              setEditing={setEditing}
+              currentUser={currentUser}
+              updateUser={updateUser}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+          </div>
+        )}
+      </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser} />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
