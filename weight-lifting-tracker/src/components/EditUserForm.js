46db75f28bb323cc120f23e34506c33b3b0f7120
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-23 09:58:14 -0400

    Fixed issue

--- weight-lifting-tracker/src/components/EditUserForm.js
+++ weight-lifting-tracker/src/components/EditUserForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EditUserForm = props => {
   const [user, setUser] = useState(props.currentUser);
@@ -8,6 +8,10 @@ const EditUserForm = props => {
 
     setUser({ ...user, [name]: value })
   };
+
+  useEffect(() => {
+    setUser(props.currentUser)
+  }, [props])
 
   return (
     <form
--- weight-lifting-tracker/src/components/UserWorkout.js
+++ weight-lifting-tracker/src/components/UserWorkout.js
@@ -5,32 +5,42 @@ import EditUserForm from './EditUserForm.js';
 
 export default function UserWorkout(props) {
 
+  // Dummy Data
   const usersData = [
     { id: 1, name: 'Tania', username: 'floppydiskette' },
     { id: 2, name: 'Craig', username: 'siliconeidolon' },
     { id: 3, name: 'Ben', username: 'benisphere' },
   ];
+
   const initialFormState = { id: null, name: '', username: '' };
 
+  // Set State
   const [users, setUsers] = useState(usersData);
+
   const [editing, setEditing] = useState(false);
+
   const [currentUser, setCurrentUser] = useState(initialFormState);
 
+  // Crud Operation
   const addUser = user => {
-    user.id = users.length + 1
-    setUsers([...users, user])
-  };
-  const editRow = user => {
-    setEditing(true)
-    setCurrentUser({ id: user.id, name: user.name, username: user.username })
-  };
-  const deleteUser = id => {
-    setUsers(users.filter(user => user.id !== id))
-  };
-  const updateUser = (id, updatedUser) => {
-  setEditing(false)
-  setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-  };
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditing(false)
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
 
   return (
     <div className="container">
