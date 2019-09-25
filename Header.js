176db118c74d54624900be22cdc9b2141e011aa2
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-24 20:21:11 -0400

    Added .toml file

---
+++ Header.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="ui centered">
+      <h1 className="ui center">Exercises</h1>
+    </header>
+  );
+}
+
