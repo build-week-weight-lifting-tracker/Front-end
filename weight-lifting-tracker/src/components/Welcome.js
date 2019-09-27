0bf21e33a6f8635940363c9594e5177e1fbb0832
adinkins2019 <afdinki@gmail.com>
2019-09-26 21:13:40 -0400

    added Welcome component

---
+++ weight-lifting-tracker/src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Welcome() {
+    return (
+        <div>
+            <h2> Welcome to Our Weight Lifting Workout Tracker</h2>
+            <p>
+                <strong>Version 0.1 Changes</strong> <br/>
+                <ul>
+                    <li>Made revisions to the website styling</li>
+                    <li>Made changes to the user profile</li>
+                </ul>
+            </p>
+        </div>
+    )
+}
