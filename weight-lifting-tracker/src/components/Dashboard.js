e28c2d06dbf5c5e3f97f55361abde5f09cb112ef
adinkins2019 <afdinki@gmail.com>
2019-09-26 22:37:06 -0400

    removed components and added panes as props

--- weight-lifting-tracker/src/components/Dashboard.js
+++ weight-lifting-tracker/src/components/Dashboard.js
@@ -1,62 +1,20 @@
 import React from 'react'
-import PersonDetails from './PersonDetails'
+
 /*import FormikWeightForm from './Journal'*/
 import {Tab} from 'semantic-ui-react'
 import Header from "./Header2"
 import Footer from './Footer'
-import ExerciseList from './ExerciseList'
-import UserWorkout from './UserWorkout'
-import {NavLink, Route, matchPath} from 'react-router-dom'
-import Welcome from './Welcome'
+import {matchPath} from 'react-router-dom'
 
-const panes = [
-    {
-      menuItem: {
-        as: NavLink,
-        content: "Home" ,
-        to: "/",
-        key: "home",
-        exact: true
-      },
-      render: () => <Route path="/"><Tab.Pane attached={false}><Welcome /></Tab.Pane></Route>
-    },
-    {
-      menuItem: {
-        as: NavLink,
-        content: "Profile",
-        to: "/profile",
-        key: "profile"
-      },
-      render: () => <Route path="/profile"><Tab.Pane attached={false}><PersonDetails /></Tab.Pane></Route>,
-    },
-    {
-      menuItem: {
-        as: NavLink,
-        content: "Workout Journal",
-        to: "/journal",
-        key: "journal"
-      },
-      render: () => <Route path="/journal"><Tab.Pane attached={false}><UserWorkout /></Tab.Pane></Route>,
-    },
-    {
-      menuItem: {
-        as: NavLink,
-        content: 'Exercise List',
-        to: "/exercises",
-        key: "exercises"
-      },
-      render: () => <Route path="/exercises"><Tab.Pane attached={false}><ExerciseList /></Tab.Pane></Route>,
-    },
-  ]
+  
 
+export default function TabNavigation({panes}){
   const defaultActiveIndex = panes.findIndex(pane => {
     return !!matchPath(window.location.pathname, {
       path: pane.menuItem.to,
       exact: true
     });
   });
-
-export default function TabNavigation(){
     return (
         <div>
         <Header />
